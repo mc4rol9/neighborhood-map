81005b1e4c08b64ed49e8b32159eff324d2340c4
mc9 <mariacarol9@gmail.com>
2017-04-21 20:25:14 -0300

    feat: Create callback for showing map

---
+++ assets/js/app.js
@@ -0,0 +1,81 @@
+// The Global variables
+var map; // the map
+
+// The function to initialize map
+function initMap() {
+    // The map styles
+    var styles = [
+      {
+        featureType: 'water',
+        stylers: [
+          { color: '#528ec7' }
+        ]
+      },{
+        featureType: 'administrative',
+        elementType: 'labels.text.stroke',
+        stylers: [
+          { color: '#ffffff' },
+          { weight: 6 }
+        ]
+      },{
+        featureType: 'administrative',
+        elementType: 'labels.text.fill',
+        stylers: [
+          { color: '#5c9028' }
+        ]
+      },{
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [
+          { color: '#5c9028' },
+          { lightness: -40 }
+        ]
+      },{
+        featureType: 'transit.station',
+        stylers: [
+          { weight: 9 },
+          { hue: '#e85113' }
+        ]
+      },{
+        featureType: 'road.highway',
+        elementType: 'labels.icon',
+        stylers: [
+          { visibility: 'off' }
+        ]
+      },{
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [
+          { lightness: 100 }
+        ]
+      },{
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [
+          { lightness: -100 }
+        ]
+      },{
+        featureType: 'poi',
+        elementType: 'geometry',
+        stylers: [
+          { visibility: 'on' },
+          { color: '#f0e4d3' }
+        ]
+      },{
+        featureType: 'road.highway',
+        elementType: 'geometry.fill',
+        stylers: [
+          { color: '#5c9028' },
+          { lightness: -25 }
+        ]
+      }
+    ];
+
+    // The constructor that creates a new map
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -23.5414317, lng: -46.6974751},
+        zoom: 15,
+        styles: styles,
+        mapTypeCOntrol: false
+    });
+}
